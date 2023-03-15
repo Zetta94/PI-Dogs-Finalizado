078e433b09b75f3ea43154f67e20b7fc724b6233
Zetta94 <manuelalexis68@hotmail.com>
2023-03-15 10:39:25 -0300

    finished

--- client/src/components/Form/validation.js
+++ client/src/components/Form/validation.js
@@ -1,14 +1,4 @@
 export default function validation (formData){
-    let regexUrl = new RegExp(
-        '^(https?:\\/\\/)?' + // protocol
-          '((([a-z\\d]([a-z\\d-][a-z\\d]))\\.)+[a-z]{2,}|' + // domain name
-          '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-          '(\\:\\d+)?(\\/[-a-z\\d%_.~+])' + // port and path
-          '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-          '(\\#[-a-z\\d_]*)?$', // fragment locator
-        'i'
-      )
-
     let error = {}
 
     if (formData.name === '') {
@@ -17,41 +7,43 @@ export default function validation (formData){
 
      if(!/^[A-Za-z\s]*$/.test(formData.name)){
          error.name = 'The name must not have any numerical characters'
-     }
+     }else if(formData.name.length > 0 && formData.name.length < 5){
+        error.name = "The name cannot have less than 5 characters";
+    }
 
     if (formData.heightMin === '') {
         error.heightMin = 'Height Minimum is required'
-    }else if (Number(formData.heightMin) > Number(formData.heightMax)) {
+    }else if (Number(formData.heightMin) >= Number(formData.heightMax)) {
         error.heightMin = 'Minimum height cannot be greater than maximum height.'
     }
 
     if (formData.heightMax === '') {
         error.heightMax = 'Height Maximum is required'
-    } else if (Number(formData.heightMax) < Number(formData.heightMin)) {
+    } else if (Number(formData.heightMax) <= Number(formData.heightMin)) {
         error.heightMax = 'Maximum height cannot be less than  minimum height.'
     }
 
     if (formData.weightMin === '') {
         error.weightMin = 'Weight Minimun is required'
-    }else if (Number(formData.weightMin) > Number(formData.weightMax)) {
+    }else if (Number(formData.weightMin) >= Number(formData.weightMax)) {
         error.weightMin = 'Minimum weight cannot be greater than maximum weight.'
     }
 
     if (formData.weightMax === '') {
         error.weightMax = 'Weight Maximun is required'
-    } else if (Number(formData.weightMax) < Number(formData.weightMin)) {
+    } else if (Number(formData.weightMax) <= Number(formData.weightMin)) {
         error.weightMax = 'Maximum weight cannot be less than  minimum weight.'
     }
 
     if (formData.lifeSpanMin === '') {
         error.lifeSpanMin = 'Life Span Minimun is required'
-    }else if (Number(formData.lifeSpanMin) > Number(formData.lifeSpanMax)) {
+    }else if (Number(formData.lifeSpanMin) >= Number(formData.lifeSpanMax)) {
         error.lifeSpanMin = 'Life Span minimum cannot be greater than life Span maximum.'
     }
 
     if (formData.lifeSpanMax === '') {
         error.lifeSpanMax = 'Life Span Maximun is required'
-    }else if (Number(formData.lifeSpanMax)< Number(formData.lifeSpanMin)) {
+    }else if (Number(formData.lifeSpanMax)<= Number(formData.lifeSpanMin)) {
         error.lifeSpanMax = 'Life Span maximum weight cannot be greater than Life Span minimum.'
     }
 
